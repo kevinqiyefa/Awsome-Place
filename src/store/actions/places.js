198fa117813e16fc08eeb0bc1ccb7f677d176244
Kevin <kevinqiyf@hotmail.com>
2018-08-19 14:10:28 -0700

    setting up the actions

---
+++ src/store/actions/places.js
@@ -0,0 +1,27 @@
+import * as actionType from './actionTypes';
+
+export const addPlace = placeName => {
+  return {
+    type: actionType.ADD_PLACE,
+    placeName
+  };
+};
+
+export const deletePlace = () => {
+  return {
+    type: actionType.DELETE_PLACE
+  };
+};
+
+export const selectPlace = key => {
+  return {
+    type: actionType.SELECT_PLACE,
+    placeKey: key
+  };
+};
+
+export const deselectPlace = key => {
+  return {
+    type: actionType.DESELECT_PLACE
+  };
+};
