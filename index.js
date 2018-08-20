0e92183394baa501f82f7e491d7e6b445cbbd96c
Kevin <kevinqiyf@hotmail.com>
2018-08-19 20:56:50 -0700

    creating the store

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import React from 'react';
+
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-reduct';
 import App from './App';
-AppRegistry.registerComponent('myfirstrnapp', () => App);
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+AppRegistry.registerComponent('myfirstrnapp', () => RNRedux);
